ea6f484c92479d264a6c5e4585344b9e60cba38c
Chris Petrevski <c_petrevski@hotmail.com>
2021-11-25 19:07:45 +1100

    changes to models

---
+++ models/Movie.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Movie extends Model {}
+
+Movie.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    movie_title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    date_released: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'project',
+  }
+);
+
+module.exports = Movie;
