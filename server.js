7d165ef5c40788db9ef38eb9d49eac2b91cb7bee
Gustavo Corchog <gugacorchog@gmail.com>
2021-11-28 13:06:38 +1100

    Layout html

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static('public'));
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
