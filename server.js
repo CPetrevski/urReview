17e068aa26327134590b258ebce16111836f7333
Chris Petrevski <c_petrevski@hotmail.com>
2021-12-06 21:04:18 +1100

    deployment27

--- server.js
+++ server.js
@@ -1,49 +1,53 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+try {
+  const path = require('path');
+  const express = require('express');
+  const session = require('express-session');
+  const exphbs = require('express-handlebars');
+  const routes = require('./controllers');
+  const helpers = require('./utils/helpers');
 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+  const sequelize = require('./config/connection');
+  const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const app = express();
+  const app = express();
 
 
-app.use(express.static('public'));
-// Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({helpers});
+  app.use(express.static('public'));
+  // Set up Handlebars.js engine with custom helpers
+  const hbs = exphbs.create({helpers});
 
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
+  const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
 
-app.use(session(sess));
+  app.use(session(sess));
 
-// Inform Express.js on which template engine to use
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+  // Inform Express.js on which template engine to use
+  app.engine('handlebars', hbs.engine);
+  app.set('view engine', 'handlebars');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+  app.use(routes);
 
-const PORT = process.env.PORT || 3000;
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+  const PORT = process.env.PORT || 3000;
+  sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
 
-// db.sequelize.sync().then(function() {
-//   app.listen(PORT, function() {
-//     console.log('App listening for db connection on Port' + PORT);
-//   });
-// });
+  // db.sequelize.sync().then(function() {
+  //   app.listen(PORT, function() {
+  //     console.log('App listening for db connection on Port' + PORT);
+  //   });
+  // });
+} catch (err) {
+  console.error(err);
+}
