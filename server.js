b8d3e24eb4380077db3f3520f7a6e05c9a70075f
Chris Petrevski <c_petrevski@hotmail.com>
2021-12-06 11:01:14 +1100

    deplpyment

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+const db = require('./models');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 // Set up Handlebars.js engine with custom helpers
@@ -40,3 +41,9 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
+// db.sequelize.sync().then(function() {
+//   app.listen(PORT, function() {
+//     console.log('App listening for db connection on Port' + PORT);
+//   });
+// });
